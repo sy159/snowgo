148e94bd9cd76dfb699dcec0c90f11fe2697b8d1
如何好听 <1597691116@qq.com>
2021-11-22 17:20:50 +0800

    feat: 配置文件struct位置调整

--- config/config.go
+++ config/config.go
@@ -1,17 +1,42 @@
 package config
 
 import (
-	redisConf "snowgo/library/cache/redis"
-	serverConf "snowgo/library/server"
 	"snowgo/utils/logger"
 
 	"github.com/spf13/viper"
 )
 
 var (
-	ServerConf serverConf.ServerConfig // ServerConf 全局server配置
-	RedisConf  redisConf.RedisConfig   // RedisConf 全局redis配置
+	ServerConf ServerConfig // ServerConf 全局server配置
+	RedisConf  RedisConfig  // RedisConf 全局redis配置
 )
+
+// ServerConfig server配置
+type ServerConfig struct {
+	IsDebug      bool   `json:"isDebug" toml:"isDebug" yaml:"isDebug"`
+	AccessLog    bool   `json:"accessLog" toml:"accessLog" yaml:"accessLog"`
+	Name         string `json:"name" toml:"name" yaml:"name"`
+	Version      string `json:"version" toml:"version" yaml:"version"`
+	Addr         string `json:"addr" toml:"addr" yaml:"addr"`
+	Port         uint32 `json:"port" toml:"port" yaml:"port"`
+	ReadTimeout  uint   `json:"readTimeout" toml:"readTimeout" yaml:"readTimeout" `
+	WriteTimeout uint   `json:"writeTimeout" toml:"writeTimeout" yaml:"writeTimeout"`
+	MaxHeaderMB  int    `json:"maxHeaderMB" toml:"maxHeaderMB" yaml:"maxHeaderMB"`
+}
+
+// RedisConfig redis配置
+type RedisConfig struct {
+	Addr         string `validate:"required" json:"addr" toml:"addr" yaml:"addr"`                      // 地址
+	Port         uint32 `validate:"required" json:"port" toml:"port" yaml:"port"`                      // 端口号
+	Password     string `json:"password" toml:"password" yaml:"password"`                              // 密码
+	DB           int    `validate:"min=0"  json:"db" toml:"db" yaml:"db"`                              // 数据库
+	DialTimeout  int    `validate:"gt=0"  json:"dialTimeout" toml:"dialTimeout" yaml:"dialTimeout"`    // 拨号超时(秒)
+	ReadTimeout  int    `validate:"gt=0"  json:"readTimeout" toml:"readTimeout" yaml:"readTimeout"`    // 读取超时(秒)
+	WriteTimeout int    `validate:"gt=0"  json:"writeTimeout" toml:"writeTimeout" yaml:"writeTimeout"` // 写入超时(秒)
+	MinIdleConns int    `validate:"gt=0"  json:"minIdleConns" toml:"minIdleConns" yaml:"minIdleConns"` // 最小空闲连接数
+	IdleTimeout  int    `validate:"gt=0"  json:"idleTimeout" toml:"idleTimeout" yaml:"idleTimeout"`    // 空闲超时(秒)
+	PoolSize     int    `validate:"gt=0"  json:"poolSize" toml:"poolSize" yaml:"poolSize"`             // 连接池最大链接数
+}
 
 // InitConf 加载所有需要配置文件
 func InitConf() {
