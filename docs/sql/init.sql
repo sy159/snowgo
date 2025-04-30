bad2d87343ab7f8979d3ea9be7fd49ebff0d191e
如何好听 <1597691116@qq.com>
2025-04-30 09:04:58 +0800

    pref: 用户表优化

--- docs/sql/init.sql
+++ docs/sql/init.sql
@@ -1,77 +1,86 @@
 # 创建user表
 CREATE TABLE `user`
 (
-    `id`         INT(11) NOT NULL AUTO_INCREMENT,
+    `id`         INT(11)     NOT NULL AUTO_INCREMENT,
     `username`   VARCHAR(64) NOT NULL COMMENT '登录名，业务唯一',
     `tel`        VARCHAR(20) NOT NULL COMMENT '手机号码',
     `nickname`   VARCHAR(60) NULL COMMENT '用户昵称',
     `password`   CHAR(64)    NOT NULL COMMENT 'pwd',
-    `status`     ENUM('Active','Disabled') NOT NULL DEFAULT 'Active'
-                 COMMENT '状态：Active 活跃，Disabled 禁用登录',
+    `status`     VARCHAR(20) NOT NULL DEFAULT 'Active' COMMENT '状态：Active 活跃，Disabled 禁用登录',
     `is_deleted` TINYINT(1)  NOT NULL DEFAULT 0 COMMENT '是否删除：0=未删除，1=已删除',
     `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     PRIMARY KEY (`id`),
-    KEY          idx_username (`username`),
-    KEY          idx_tel (`tel`),
-    KEY          idx_status (`status`),
-    KEY          idx_is_deleted (`is_deleted`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户表';
+    KEY idx_username (`username`),
+    KEY idx_tel (`tel`),
+    KEY idx_status (`status`),
+    KEY idx_is_deleted (`is_deleted`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='用户表';
 
 # 创建角色表
 CREATE TABLE `role`
 (
-    `id`          INT(11)       NOT NULL AUTO_INCREMENT,
-    `code`        VARCHAR(64) NOT NULL COMMENT '角色代码，如 admin、normal',
+    `id`          INT(11)      NOT NULL AUTO_INCREMENT,
+    `code`        VARCHAR(64)  NOT NULL COMMENT '角色代码，如 admin、normal',
     `name`        VARCHAR(128) NULL COMMENT '前端展示用名称',
-    `description` TEXT NULL COMMENT '角色描述',
+    `description` TEXT         NULL COMMENT '角色描述',
     `created_at`  DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     `updated_at`  DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_name` (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='角色表';
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='角色表';
 
 # 创建菜单权限表
 CREATE TABLE `menu`
 (
-    `id`         INT(11)       NOT NULL AUTO_INCREMENT,
-    `parent_id`  INT(11)      NOT NULL  DEFAULT 0   COMMENT '父级菜单，0=根节点',
-    `menu_type`  ENUM('Dir','Menu','Btn') NOT NULL COMMENT '类型：Dir/菜单目录, Menu/页面菜单, Btn/按钮操作',
-    `name`       VARCHAR(64) NOT NULL COMMENT '节点名称（前端显示）',
-    `path`       VARCHAR(128) NULL   COMMENT '前端路由路径，仅 Dir/Menu 生效',
-    `icon`       VARCHAR(64) NULL   COMMENT '节点图标，仅 Dir/Menu 生效',
-    `perms`      VARCHAR(100) NULL   COMMENT '权限标识，如 system:user:add，仅 Btn生效',
-    `order_num`  INT         NOT NULL DEFAULT 0 COMMENT '排序号',
-    `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `id`         INT(11)                   NOT NULL AUTO_INCREMENT,
+    `parent_id`  INT(11)                   NOT NULL DEFAULT 0 COMMENT '父级菜单，0=根节点',
+    `menu_type`  ENUM ('Dir','Menu','Btn') NOT NULL COMMENT '类型：Dir/菜单目录, Menu/页面菜单, Btn/按钮操作',
+    `name`       VARCHAR(64)               NOT NULL COMMENT '节点名称（前端显示）',
+    `path`       VARCHAR(128)              NULL COMMENT '前端路由路径，仅 Dir/Menu 生效',
+    `icon`       VARCHAR(64)               NULL COMMENT '节点图标，仅 Dir/Menu 生效',
+    `perms`      VARCHAR(100)              NULL COMMENT '权限标识，如 system:user:add，仅 Btn生效',
+    `order_num`  INT                       NOT NULL DEFAULT 0 COMMENT '排序号',
+    `created_at` DATETIME(6)               NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6)               NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='菜单权限表';
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='菜单权限表';
 
 # 用户-角色关联表
 CREATE TABLE `user_role`
 (
-    `id`         BIGINT NOT NULL AUTO_INCREMENT,
-    `user_id`    INT(11) NOT NULL COMMENT '用户ID',
-    `role_id`    INT(11) NOT NULL COMMENT '角色ID',
+    `id`         BIGINT      NOT NULL AUTO_INCREMENT,
+    `user_id`    INT(11)     NOT NULL COMMENT '用户ID',
+    `role_id`    INT(11)     NOT NULL COMMENT '角色ID',
     `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     PRIMARY KEY (`id`),
-    KEY          `idx_user_id` (`user_id`),
-    KEY          `idx_role_id` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户-角色关联表';
+    KEY `idx_user_id` (`user_id`),
+    KEY `idx_role_id` (`role_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='用户-角色关联表';
 
 # 创建角色-菜单权限表
 CREATE TABLE `role_menu`
 (
-    `id`         BIGINT NOT NULL AUTO_INCREMENT,
-    `role_id`    INT(11) NOT NULL COMMENT '角色ID',
-    `menu_id`    INT(11) NOT NULL COMMENT '菜单或按钮ID',
+    `id`         BIGINT      NOT NULL AUTO_INCREMENT,
+    `role_id`    INT(11)     NOT NULL COMMENT '角色ID',
+    `menu_id`    INT(11)     NOT NULL COMMENT '菜单或按钮ID',
     `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     PRIMARY KEY (`id`),
-    KEY          `idx_role_id` (`role_id`),
-    KEY          `idx_menu_id` (`menu_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='角色-菜单权限表';
+    KEY `idx_role_id` (`role_id`),
+    KEY `idx_menu_id` (`menu_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='角色-菜单权限表';
 
 # 创建操作日志表
 CREATE TABLE `operation_log`
@@ -84,72 +93,122 @@ CREATE TABLE `operation_log`
     `resource_id`   INT         NOT NULL COMMENT '资源ID，如 user 表的主键 ID',
     `action`        VARCHAR(10) NOT NULL DEFAULT 'Create' COMMENT '操作类型：Create/Update/Delete，默认 Create',
     `trace_id`      VARCHAR(64) NULL COMMENT '链路id',
-    `before_data`   JSON NULL                   COMMENT '修改前数据快照（仅 update/delete 时填）',
-    `after_data`    JSON NULL                   COMMENT '修改后数据快照（仅 create/update 时填）',
-    `description`   TEXT NULL                   COMMENT '描述',
-    `ip`            VARCHAR(45) NULL                   COMMENT '客户端 IP',
-    `created_at`    DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `before_data`   JSON        NULL COMMENT '修改前数据快照（仅 update/delete 时填）',
+    `after_data`    JSON        NULL COMMENT '修改后数据快照（仅 create/update 时填）',
+    `description`   TEXT        NULL COMMENT '描述',
+    `ip`            VARCHAR(45) NULL COMMENT '客户端 IP',
+    `created_at`    DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     PRIMARY KEY (`id`),
-    KEY             `idx_operator_id` (`operator_id`),
-    KEY             `idx_trace_id` (`trace_id`),
-    KEY             `idx_resource` (`resource`,`resource_id`),
-    KEY             `idx_created_at` (`created_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='操作日志表';
+    KEY `idx_operator_id` (`operator_id`),
+    KEY `idx_trace_id` (`trace_id`),
+    KEY `idx_resource` (`resource`, `resource_id`),
+    KEY `idx_created_at` (`created_at`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='操作日志表';
 
 # 插入测试数据
 # 用户数据
-INSERT INTO `user` (`username`, `tel`, `nickname`, `password`, `status`, `is_deleted`) VALUES ('admin', '18712345678', '如何好听', '$2a$10$XqU5GKb6wbGXjckKxQtMF.b8nn6MlC17tk2Y.ap//n8swLOQ4fZwO', 'Active', 0);
+INSERT INTO `user` (`username`, `tel`, `nickname`, `password`, `status`, `is_deleted`)
+VALUES ('admin', '18712345678', '如何好听', '$2a$10$XqU5GKb6wbGXjckKxQtMF.b8nn6MlC17tk2Y.ap//n8swLOQ4fZwO', 'Active',
+        0);
 
 # 菜单数据
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (1, 0, 'Dir', '账号管理', '', '', '', 1);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (2, 1, 'Menu', '用户管理', '/account/user', 'fa fa-user-o', '', 1);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (3, 2, 'Btn', '用户列表', '', '', 'account:user:list', 1);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (4, 2, 'Btn', '用户详情', '', '', 'account:user:detail', 2);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (5, 2, 'Btn', '添加用户', '', '', 'account:user:create', 3);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (6, 2, 'Btn', '更新用户', '', '', 'account:user:update', 4);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (7, 2, 'Btn', '删除用户', '', '', 'account:user:delete', 5);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (8, 2, 'Btn', '重置密码', '', '', 'account:user:reset_pwd', 6);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (9, 1, 'Menu', '角色管理', '/account/role', 'fa fa-user-secret', '', 2);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (10, 9, 'Btn', '角色列表', '', '', 'account:role:list', 1);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (11, 9, 'Btn', '角色详情', '', '', 'account:role:detail', 2);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (12, 9, 'Btn', '添加角色', '', '', 'account:role:create', 3);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (13, 9, 'Btn', '更新角色', '', '', 'account:role:update', 4);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (14, 9, 'Btn', '删除角色', '', '', 'account:role:delete', 5);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (15, 1, 'Menu', '菜单管理', '/account/menu', 'fa fa-th-list', '', 3);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (16, 15, 'Btn', '菜单列表', '', '', 'account:menu:list', 1);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (17, 15, 'Btn', '添加菜单', '', '', 'account:menu:create', 2);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (18, 15, 'Btn', '更新菜单', '', '', 'account:menu:update', 3);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (19, 15, 'Btn', '删除菜单', '', '', 'account:menu:delete', 4);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (20, 0, 'Dir', '系统管理', '', '', '', 2);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (21, 20, 'Menu', '操作日志管理', '/system/operation-log', 'fa fa-pencil-square-o', '', 1);
-INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`) VALUES (22, 21, 'Btn', '操作日志列表', '', '', 'system:operation-log:list', 1);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (1, 0, 'Dir', '账号管理', '', '', '', 1);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (2, 1, 'Menu', '用户管理', '/account/user', 'fa fa-user-o', '', 1);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (3, 2, 'Btn', '用户列表', '', '', 'account:user:list', 1);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (4, 2, 'Btn', '用户详情', '', '', 'account:user:detail', 2);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (5, 2, 'Btn', '添加用户', '', '', 'account:user:create', 3);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (6, 2, 'Btn', '更新用户', '', '', 'account:user:update', 4);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (7, 2, 'Btn', '删除用户', '', '', 'account:user:delete', 5);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (8, 2, 'Btn', '重置密码', '', '', 'account:user:reset_pwd', 6);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (9, 1, 'Menu', '角色管理', '/account/role', 'fa fa-user-secret', '', 2);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (10, 9, 'Btn', '角色列表', '', '', 'account:role:list', 1);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (11, 9, 'Btn', '角色详情', '', '', 'account:role:detail', 2);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (12, 9, 'Btn', '添加角色', '', '', 'account:role:create', 3);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (13, 9, 'Btn', '更新角色', '', '', 'account:role:update', 4);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (14, 9, 'Btn', '删除角色', '', '', 'account:role:delete', 5);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (15, 1, 'Menu', '菜单管理', '/account/menu', 'fa fa-th-list', '', 3);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (16, 15, 'Btn', '菜单列表', '', '', 'account:menu:list', 1);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (17, 15, 'Btn', '添加菜单', '', '', 'account:menu:create', 2);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (18, 15, 'Btn', '更新菜单', '', '', 'account:menu:update', 3);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (19, 15, 'Btn', '删除菜单', '', '', 'account:menu:delete', 4);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (20, 0, 'Dir', '系统管理', '', '', '', 2);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (21, 20, 'Menu', '操作日志管理', '/system/operation-log', 'fa fa-pencil-square-o', '', 1);
+INSERT INTO `menu` (`id`, `parent_id`, `menu_type`, `name`, `path`, `icon`, `perms`, `order_num`)
+VALUES (22, 21, 'Btn', '操作日志列表', '', '', 'system:operation-log:list', 1);
 
 # 角色数据
-INSERT INTO `role` (`id`, `code`, `name`, `description`) VALUES (1, 'admin', '管理员', '平台管理员角色');
+INSERT INTO `role` (`id`, `code`, `name`, `description`)
+VALUES (1, 'admin', '管理员', '平台管理员角色');
 
 # 角色菜单关联数据
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 1);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 2);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 3);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 4);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 5);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 6);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 7);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 8);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 9);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 10);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 11);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 12);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 13);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 14);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 15);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 16);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 17);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 18);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 19);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 20);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 21);
-INSERT INTO `role_menu` (`role_id`, `menu_id`) VALUES (1, 22);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 1);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 2);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 3);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 4);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 5);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 6);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 7);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 8);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 9);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 10);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 11);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 12);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 13);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 14);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 15);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 16);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 17);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 18);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 19);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 20);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 21);
+INSERT INTO `role_menu` (`role_id`, `menu_id`)
+VALUES (1, 22);
 
 # 用户角色关联数据
-INSERT INTO `user_role` (`user_id`, `role_id`) VALUES (1, 1);
+INSERT INTO `user_role` (`user_id`, `role_id`)
+VALUES (1, 1);
--- internal/dal/model/t_user.gen.go
+++ internal/dal/model/t_user.gen.go
@@ -13,12 +13,12 @@ const TableNameUser = "user"
 // User 用户表
 type User struct {
 	ID        int32      `gorm:"column:id;type:int(11);primaryKey;autoIncrement:true" json:"id"`
-	Username  string     `gorm:"column:username;type:varchar(64);not null;index:idx_username,priority:1;comment:登录名，业务唯一" json:"username"`                                          // 登录名，业务唯一
-	Tel       string     `gorm:"column:tel;type:varchar(20);not null;index:idx_tel,priority:1;comment:手机号码" json:"tel"`                                                             // 手机号码
-	Nickname  *string    `gorm:"column:nickname;type:varchar(60);comment:用户昵称" json:"nickname"`                                                                                     // 用户昵称
-	Password  string     `gorm:"column:password;type:char(64);not null;comment:pwd" json:"password"`                                                                                // pwd
-	Status    *string    `gorm:"column:status;type:enum('Active','Disabled');not null;index:idx_status,priority:1;default:Active;comment:状态：Active 活跃，Disabled 禁用登录" json:"status"` // 状态：Active 活跃，Disabled 禁用登录
-	IsDeleted bool       `gorm:"column:is_deleted;type:tinyint(1);not null;index:idx_is_deleted,priority:1;comment:是否删除：0=未删除，1=已删除" json:"is_deleted"`                             // 是否删除：0=未删除，1=已删除
+	Username  string     `gorm:"column:username;type:varchar(64);not null;index:idx_username,priority:1;comment:登录名，业务唯一" json:"username"`                            // 登录名，业务唯一
+	Tel       string     `gorm:"column:tel;type:varchar(20);not null;index:idx_tel,priority:1;comment:手机号码" json:"tel"`                                               // 手机号码
+	Nickname  *string    `gorm:"column:nickname;type:varchar(60);comment:用户昵称" json:"nickname"`                                                                       // 用户昵称
+	Password  string     `gorm:"column:password;type:char(64);not null;comment:pwd" json:"password"`                                                                  // pwd
+	Status    *string    `gorm:"column:status;type:varchar(20);not null;index:idx_status,priority:1;default:Active;comment:状态：Active 活跃，Disabled 禁用登录" json:"status"` // 状态：Active 活跃，Disabled 禁用登录
+	IsDeleted bool       `gorm:"column:is_deleted;type:tinyint(1);not null;index:idx_is_deleted,priority:1;comment:是否删除：0=未删除，1=已删除" json:"is_deleted"`               // 是否删除：0=未删除，1=已删除
 	CreatedAt *time.Time `gorm:"column:created_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"created_at"`
 	UpdatedAt *time.Time `gorm:"column:updated_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"updated_at"`
 }
