8f9e61c6e62872ea89660b99babc52c888431443
如何好听 <1597691116@qq.com>
2025-12-17 17:49:21 +0800

    feat: 队列常量

---
+++ internal/constant/mq.go
@@ -0,0 +1,15 @@
+package constant
+
+const (
+	// NormalExchange exchange 相关
+	NormalExchange  = "snow.normal.exchange"
+	DelayedExchange = "snow.delayed.exchange"
+)
+
+const (
+	// ExampleNormalQueue queue routingKey 相关
+	ExampleNormalQueue       = "example.normal.queue"
+	ExampleNormalRoutingKey  = "example.normal"
+	ExampleDelayedQueue      = "example.delayed.queue"
+	ExampleDelayedRoutingKey = "example.delayed"
+)
